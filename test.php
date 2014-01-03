be18016a0e84ec7fe8c96c9195be66cd42069b91
litton5211 <litton.zheng@gmail.com>
2014-01-03 18:24:06 +0800

    非付费

--- test.php
+++ test.php
@@ -1,2 +1,14 @@
+
 <?php
-echo 888;
+$var = 0;
+
+// 结果为 true，因为 $var 为空
+if (empty($var)) {  
+    echo '$var is either 0 or not set at all';
+}
+
+// 结果为 false，因为 $var 已设置
+if (!isset($var)) { 
+    echo '$var is not set at all';
+}
+?>
